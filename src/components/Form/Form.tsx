9ebb7a7367e0e2084648bde382dfd879a544b8d0
Domagoj <paulic.domagoj@gmail.com>
2024-05-14 09:11:55 +0200

    use controlled text input

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -1,7 +1,9 @@
 import * as React from 'react';
 import { FormEvent } from 'react';
+import styled from 'styled-components';
 
 const Form: React.FC = () => {
+  const [name, setName] = React.useState('');
   // Use FormData
   const handleSubmit = (event: FormEvent) => {
     event.preventDefault();
@@ -14,10 +16,26 @@ const Form: React.FC = () => {
 
   return (
     <form onSubmit={handleSubmit}>
-      <label htmlFor="username">Select a username;</label>
-      <input type="text" id="username" name="username" />
+      <label htmlFor="name-filed">Name</label>
+      <input
+        type="text"
+        id="name-filed"
+        value={name}
+        onChange={(event) => {
+          setName(event.target.value);
+        }}
+      />
+      <ValueContainer>
+        <strong>Current value:</strong>
+        {name || '(empty)'}
+      </ValueContainer>
     </form>
   );
 };
 
 export default Form;
+
+const ValueContainer = styled.p`
+  display: flex;
+  flex-direction: column;
+`;

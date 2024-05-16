a79ddc4e9ed5b33e4cf41d1331cdd6c100e02559
Domagoj <paulic.domagoj@gmail.com>
2024-05-16 07:53:26 +0200

    extract component, amend types

---
+++ src/components/FormControls/CountryPicker.tsx
@@ -0,0 +1,70 @@
+import styled from 'styled-components';
+import { Country, CountryCode } from './FormControls';
+import { COUNTRIES } from './data';
+
+type CountryPickerProps = {
+  countires: Country[];
+  countryCode: string;
+  setCountryCode: (countryCode: CountryCode) => void;
+};
+
+const CountryPicker: React.FC<CountryPickerProps> = ({
+  countires,
+  countryCode,
+  setCountryCode,
+}) => {
+  return (
+    <>
+      <StyledFieldset>
+        <legend>Shipping info</legend>
+        <label htmlFor="country">Country</label>
+        <br />
+        <Select
+          required
+          name="country"
+          id="country"
+          value={countryCode}
+          onChange={(event) => {
+            setCountryCode(event.target.value as CountryCode);
+          }}
+        >
+          <option value="">-- Select a country --</option>
+          <optgroup label="Country">
+            {countires.map((country, index) => (
+              <option value={country.value} key={index}>
+                {country.name}
+              </option>
+            ))}
+          </optgroup>
+        </Select>
+      </StyledFieldset>
+      <ValueDisplay>
+        Selected country:{' '}
+        {countryCode !== ''
+          ? COUNTRIES[countryCode as CountryCode]
+          : 'No selection'}
+      </ValueDisplay>
+    </>
+  );
+};
+
+export default CountryPicker;
+
+const StyledFieldset = styled.fieldset`
+  border-radius: 4px;
+`;
+
+const Select = styled.select`
+  width: 100%;
+  height: 2.5rem;
+  background-color: white;
+  border: 1px solid black;
+  border-radius: 2px;
+  padding: 0px 6px;
+`;
+
+const ValueDisplay = styled.p`
+  border-radius: 4px;
+  padding: 16px 24px;
+  background-color: rgba(238, 238, 124, 0.686);
+`;
--- src/components/FormControls/FormControls.tsx
+++ src/components/FormControls/FormControls.tsx
@@ -2,95 +2,39 @@ import * as React from 'react';
 import { FormEvent, useState } from 'react';
 import styled from 'styled-components';
 import { COUNTRIES } from './data';
+import CountryPicker from './CountryPicker';
 
-type CountryValues = keyof typeof COUNTRIES;
-type CountryNames = (typeof COUNTRIES)[CountryValues];
+export type CountryCode = keyof typeof COUNTRIES;
+export type CountryName = (typeof COUNTRIES)[CountryCode];
+
+export type Country = {
+  value: CountryCode;
+  name: CountryName;
+};
 
 const FormControls: React.FC = () => {
-  const [country, setCountry] = useState<CountryNames | ''>('');
-  const [selectedOption, setSelectedOption] = useState('red');
-  const [value, setValue] = useState('no');
+  const [countryCode, setCountryCode] = useState<string>('HR');
+
   const handleSubmit = (event: FormEvent) => {
     event?.preventDefault();
   };
 
-  const countryList = Object.entries(COUNTRIES).map((country) => {
+  const countries: Country[] = Object.entries(COUNTRIES).map((country) => {
     return {
-      value: country[0],
+      value: country[0] as CountryCode,
       name: country[1],
     };
   });
 
-  console.log({ COUNTRIES, countryList });
   return (
     <Form onSubmit={handleSubmit}>
       <h2>Form Controls</h2>
-
-      <label htmlFor="textarea">Text area</label>
-      <textarea name="textarea-tag" id="textarea"></textarea>
-
-      <label htmlFor="select">Select</label>
-      <StyledFieldset>
-        <legend>Choose an option</legend>
-        <select
-          name="select-tag"
-          id="select"
-          value={selectedOption}
-          onChange={(event) => setSelectedOption(event.target.value)}
-        >
-          <option value="red">Red</option>
-          <option value="green">Green</option>
-          <option value="blue">Blue</option>
-        </select>
-      </StyledFieldset>
-      <ValueDisplay>Selected value: {selectedOption}</ValueDisplay>
-
-      <label htmlFor="radio-buttons">Radio buttons</label>
-      <StyledFieldset id="radio-buttons">
-        <legend>Do you agree?</legend>
-        <input
-          type="radio"
-          name="agreed-to-terms"
-          id="agreed-yes"
-          value="yes"
-          checked={value === 'yes'}
-          onChange={(event) => setValue(event.target.value)}
-        />{' '}
-        <label htmlFor="agreed-yes">Yes</label>
-        <br />
-        <input
-          type="radio"
-          name="agreed-to-terms"
-          id="agreed-no"
-          value="no"
-          checked={value === 'no'}
-          onChange={(event) => setValue(event.target.value)}
-        />{' '}
-        <label htmlFor="agreed-no">No</label>
-      </StyledFieldset>
-
-      <StyledFieldset>
-        <legend>Shipping info</legend>
-        <label htmlFor="country">Country</label>
-        <br />
-        <Select
-          name="country"
-          id="country"
-          onChange={(event) => {
-            setCountry(COUNTRIES[event.target.value as CountryValues]);
-          }}
-        >
-          <option value={country}>-- Select a country --</option>
-          <optgroup label="Country">
-            {countryList.map((country, index) => (
-              <option value={country.value} key={index}>
-                {country.name}
-              </option>
-            ))}
-          </optgroup>
-        </Select>
-      </StyledFieldset>
-      <ValueDisplay>Selected country: {country}</ValueDisplay>
+      <CountryPicker
+        countires={countries}
+        countryCode={countryCode}
+        setCountryCode={setCountryCode}
+      />
+      <button type="submit">SUBMIT</button>
     </Form>
   );
 };
@@ -104,23 +48,4 @@ const Form = styled.form`
   padding: 12px;
   border: 1px solid grey;
   border-radius: 4px;
-`;
-
-const StyledFieldset = styled.fieldset`
-  border-radius: 4px;
-`;
-
-const Select = styled.select`
-  width: 100%;
-  height: 2.5rem;
-  background-color: white;
-  border: 1px solid black;
-  border-radius: 2px;
-  padding: 0px 6px;
-`;
-
-const ValueDisplay = styled.p`
-  border-radius: 4px;
-  padding: 16px 24px;
-  background-color: rgba(238, 238, 124, 0.686);
 `;

535e028ba025a25bfc49205aeeb997d4248e7363
Domagoj <paulic.domagoj@gmail.com>
2024-05-30 19:48:39 +0200

    add visually hidden component

---
+++ src/components/VisuallyHidden/VisuallyHidden.tsx
@@ -0,0 +1,20 @@
+import { ReactNode } from 'react';
+import styled from 'styled-components';
+
+const VisuallyHidden: React.FC<{ children: ReactNode }> = ({ children }) => {
+  return <HiddenContent>{children}</HiddenContent>;
+};
+
+export default VisuallyHidden;
+
+const HiddenContent = styled.span`
+  display: 'inline-block';
+  position: 'absolute';
+  overflow: 'hidden';
+  clip: 'rect(0 0 0 0)';
+  height: 1;
+  width: 1;
+  margin: -1;
+  padding: 0;
+  border: 0;
+`;

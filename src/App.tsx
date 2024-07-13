72141aebe7f1af1de2f16b84a4b8a40f47f22a05
Domagoj <paulic.domagoj@gmail.com>
2024-07-13 11:27:18 +0200

    add global styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import DisplayCards from './components/Card/DisplayCards';
 import Layout from './components/Layout';
 
+
+
 function App() {
   return (
     <Layout>
--- src/components/Card/DisplayCards.tsx
+++ src/components/Card/DisplayCards.tsx
@@ -1,11 +1,39 @@
 import React from 'react';
-import styled from 'styled-components';
+import styled, { createGlobalStyle } from 'styled-components';
 import UserProfileCard from './UserProfileCard';
 import ProductInfoCard from './ProductInfoCard';
+
+const GlobalStyles = createGlobalStyle`
+  html {
+  /*
+    These CSS variables establish different
+    values to be used with the “box-shadow”
+    property, to simulate elements being
+    at different elevation levels.
+  */
+  --elevation-1:
+    0px 0.5px 1px hsl(0deg 0% 0% / 0.2),
+    0px 1px 2px hsl(0deg 0% 0% / 0.15),
+    0px 2px 4px hsl(0deg 0% 0% / 0.1);
+  --elevation-2:
+    0px 1px 2px hsl(0deg 0% 0% / 0.2),
+    0px 2px 4px hsl(0deg 0% 0% / 0.15),
+    0px 4px 8px hsl(0deg 0% 0% / 0.15),
+    0px 8px 16px hsl(0deg 0% 0% / 0.1);
+  --elevation-3:
+    0px 1px 2px hsl(0deg 0% 0% / 0.15),
+    0px 2px 4px hsl(0deg 0% 0% / 0.15),
+    0px 4px 8px hsl(0deg 0% 0% / 0.125),
+    0px 8px 16px hsl(0deg 0% 0% / 0.125),
+    0px 16px 32px hsl(0deg 0% 0% / 0.1),
+    0px 32px 64px hsl(0deg 0% 0% / 0.1);
+  }
+`;
 
 const DisplayCards: React.FC = () => {
   return (
     <Wrapper>
+      <GlobalStyles />
       <UserProfileCard />
       <ProductInfoCard />
     </Wrapper>

0179f8fbcca175fe7c7c25ef8c7204fb0eb76383
Domagoj <paulic.domagoj@gmail.com>
2024-07-13 11:00:25 +0200

    add card component

---
+++ src/components/Card/Card.tsx
@@ -0,0 +1,7 @@
+import * as React from 'react';
+
+function Card() {
+  return <div></div>;
+}
+
+export default Card;
--- src/components/ProductDetails/index.ts
+++ src/components/ProductDetails/index.ts
@@ -1,2 +1,4 @@
 export * from './Product';
 export { default } from './Product';
+
+
